e9ef8c2c52caae44448277581c4bd6d87bd7710c
Senthil Kumar P <senthilm784@gmail.com>
2023-03-12 23:17:13 +0530

    Pages Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
+import ProductDetail from "./Pages/ProductDetail";
+import Cart from "./Pages/Cart";
+import Checkout from "./Pages/Checkout";
+import Dashboard from "./Pages/Dashboard";
+import AdminDashboard from "./Pages/AdminDashboard";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/products" element={<ProductList />} />
+            <Route exact path="/products/:id" element={<ProductDetail />} />
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/admin-dashboard" element={<AdminDashboard />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
