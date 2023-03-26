2b81e92eef61b0dd9bfd986eaeda715be4c1ac35
Senthil Kumar P <senthilm784@gmail.com>
2023-03-27 00:08:16 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -8,6 +8,13 @@ require("dotenv").config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
