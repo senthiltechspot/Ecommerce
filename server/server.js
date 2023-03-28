f66086973c89d369855d514351caa1e520e3ed9b
Senthil Kumar P <senthilm784@gmail.com>
2023-03-28 11:42:14 +0530

    Server Error Fixed

--- server/server.js
+++ server/server.js
@@ -8,12 +8,7 @@ require("dotenv").config();
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 connectDB();
 
